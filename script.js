50773b6585dbb3c71f0e0ba00c606d539ab297f5
SanskarOmer <sanskaromer7@gmail.com>
2024-11-06 23:30:51 +0530

    added colorpicker  functionality

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const canvas = document.querySelector("canvas"),
+const canvas = document.querySelector("canvas");
 
-ctx = canvas.getContext("2d"),
-toolBtns = document.querySelectorAll(".tool"),
+ctx = canvas.getContext("2d");
+toolBtns = document.querySelectorAll(".tool");
 fillColor = document.querySelector("#fill-color");
 sizeSlider = document.querySelector("#size-slider");
-colorBtns = document.querySelector("#size-slider");
+colorBtns = document.querySelectorAll(".colors .option");
+colorPicker = document.querySelector("#color-picker");
 
 let prevMouseX,prevMouseY;
 let isDrawing=false;
 let brushWidth=5;
 let selectedTool ="brush";
+let selectedColor="#000"
 
 window.addEventListener("load", () => {
-    // setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
+    // setting canvas width/height offset width/height returns viewable width/height of an element. 
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 });
@@ -24,7 +26,8 @@ const startDraw = (e) =>{
     ctx.moveTo(prevMouseX, prevMouseY); // move to the point where mouse is clicked
     ctx.beginPath(); // start drawing
     ctx.lineWidth = brushWidth; // setting line width
-    ctx.strokeStyle = "black"; // setting line color
+    ctx.strokeStyle = selectedColor;
+    ctx.fillStyle = selectedColor;// setting line color
     // copying canvas data & passing as snapshot value.. this avoids dragging the image
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
@@ -78,6 +81,22 @@ toolBtns.forEach(btn => {
         btn.classList.add("active");
         selectedTool=btn.id;
     });
+});
+
+colorBtns.forEach(btn => {
+    btn.addEventListener("click", () => { // adding click event to all color button
+    // removing selected class from the previous option and adding on current clicked option
+        document.querySelector(".options .selected").classList.remove("selected");
+        btn.classList.add("selected");
+        // passing selected btn background color as selectedColor value
+        selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+    });
+});
+
+colorPicker.addEventListener("change", () => {
+    // passing picked color value from color picker to last color btn background
+    colorPicker.parentElement.style.background = colorPicker.value;
+    colorPicker.parentElement.click();
 });
 
 sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value); // passing slider value as brushSize
